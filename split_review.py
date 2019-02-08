05217942bf1e9ce4231e439d518d604c3016ebab
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-02-08 14:51:50 +0900

    improve code

--- split_review.py
+++ split_review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import re, sys
 import MeCab
 import pandas as pd
 
@@ -8,33 +8,36 @@ import pandas as pd
 
 # returns corpus ["", "", ..., ""]
 # 分かち書き
-def devide_with_spaces():
-    return 
 
-def main():
-    # first arg => input filename, second arg => output filename
-    args = sys.argv
-    # ファイル読み込み with panda
-    df = pd.read_csv(args[1], skiprows=[0], sep=',')
-
-    f = open(args[2], 'w')
+def newline(input_filename, output_filename):
+    df = pd.read_csv(input_filename, sep=',')
+    f = open(output_filename, 'w')
     for _, row in df.iterrows():
         review_text = row[3]
         if type(review_text) is not str:
             continue
-        review_text.replace('。', '。\n')
+        review_text = re.sub(r'。', r'。\n', review_text)
+        review_text = re.sub(r'\n{2,}', '\n', review_text)
         f.write(review_text)
     f.close()
+    return
 
-    # 分かち書き
-    tagger = MeCab.Tagger('-OWakati')
+def wakati(input_filename, output_filename):
+    tagger = MeCab.Tagger('-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+    with open(input_filename, 'r') as f:
+        wakati_f = open(output_filename, 'w')
+        for line in f:
+            wakati = tagger.parse(line)
+            wakati_f.write(wakati)
+        wakati_f.close()
 
-    text = open(args[2], 'r')
-    wakati = tagger.parse(text)
 
-    f = open(args[3], 'w')
-    f.write(wakati)
-    f.close()
+def main():
+    # args[1] => csv, args[2] => devided with new line, args[3] => wakati gaki text
+    args = sys.argv
+    newline(args[1], args[2])
+    if args[3] is not None:
+        wakati(args[2], args[3])
 
 
 if __name__ == '__main__':
